8a42397d8b782445726d7e374b39aea51fbe13c4
Miha Lunar <mlunar@gmail.com>
2026-01-25 19:09:26 +0100

    Remove unused token_ref.py file

--- token_ref.py
+++
@@ -1,8 +0,0 @@
-from cliponnx.simple_tokenizer import SimpleTokenizer
-samples=['a photo of a cat','Hello, world!','An astronaut riding a horse']
-st=SimpleTokenizer()
-print('Python reference:')
-for s in samples:
-    ids=st.encode(s)
-    print(repr(s),'=>',ids,'len=',len(ids))
-print('Special',st.encoder['<|startoftext|>'],st.encoder['<|endoftext|>'])
